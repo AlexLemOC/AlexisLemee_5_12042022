ee5e9b8b3981166e8dd225bcd5280a9cb85a0bcd
Alexis Lemée <96991663+AlexLemOC@users.noreply.github.com>
2022-04-12 16:23:59 +0200

    projet start

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -49,7 +49,7 @@ async function insertInSection() {
 
 // Récupération des produits de l'api
 //------------------------------------------------------------------------
-fetch("http://localhost:3000/api/products")
+/*fetch("http://localhost:3000/api/products")
   // quand tu as la réponse donne le résultat en json.
   .then((res) => res.json())
   // ce que l'on a reçu et qui a été traité en json sera appelé objetProduits
@@ -71,10 +71,10 @@ function lesKanaps(index) {
   // déclaration de variable de la zone d'article
   let zoneArticle = document.querySelector("#items");
   // boucle pour chaque indice(nommé 'article') dans index
-  for (let article of index) {
+  for (let article of index) {*/
     /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin      produit + paramètres(son id);*/
  
-    zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+    /*zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
     <article>
       <img src="${article.imageUrl}" alt="${article.altTxt}">
       <h3 class="productName">${article.name}</h3>
@@ -82,4 +82,4 @@ function lesKanaps(index) {
     </article>
   </a>`;
   }
-}
+}*/
