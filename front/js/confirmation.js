f71084e72d4956a2f38d579c8ca837a8a0fdfe8d
Alexis Lemée <96991663+AlexLemOC@users.noreply.github.com>
2022-10-24 13:26:12 +0200

    fin projet

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,24 +1,21 @@
-//let order = window.localStorage.getItem("orderId"); //récupération de l'id de commande
-//console.log(order);
+// On récupère l'id qui a été déposé dans l'URL
 
-var str = window.location.href;
-var url = new URL(str);
-var idCommande = url.searchParams.get("_cde");
-console.log(idCommande);
+let str = window.location.href;
+let url = new URL(str);
+let urlSearchParamId = url.searchParams.get("id");
+
+// On utilise une fonction pour utiliser l'id afin de placer le n° de commande
+// Ce numéro avait été récupéré via innerHTML
 
 function confirm(){
-  //const orderId = document.getElementById("orderId");
-  //orderId.innerText = order;//.slice(3,13);
-  let zoneConf = document.querySelector(".confirmation");
-  if (idCommande === null){
-    idCommande = '';//On enlève l'affichage du mot "null" pour laisser un vide quand c'est vide.
-  }//On modifie le message pour qu'il soit plus poli et on insère le numéro de commande
-  zoneConf.innerHTML = `<p>Merci pour votre commande ! <br>Votre numéro de commande est : <span id="orderId">${idCommande}</span></p>`;//.slice(3,13);
-  
-  //localStorage.clear(); //Suppression du numéro de commande
+  let orderId = document.querySelector(".confirmation");
+  orderId.innerHTML = `<p>Merci pour vottre commande ! <br>Votre numéro de commande est : <span id="orderId">${urlSearchParamId}</span><br>Conservez-le car nous ne le gardons pas en mémoire.</p>`;
+  if (urlSearchParamId === null ){
+    window.location.href ="index.html";
+    window.alert("Erreur ! Vous devez posséder un panier pour accder à cette page.");
+  }
+  //Suppression du numéro de commande comme demandé dans l'étape 11
   history.pushState(null, '', 'confirmation.html');
-  console.log(localStorage);
 }
 
 confirm();
-
