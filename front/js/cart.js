07a7861e395b7f7f57b829aa4e3fe9d68aafc2e8
Alexis Lemée <96991663+AlexLemOC@users.noreply.github.com>
2023-03-16 17:04:02 +0100

    Final update

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,138 +1,302 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <title>Cart</title>
+// On récupère les objets "produit" dans le local storage
 
-    <meta charset="utf-8">
-    <meta name="description" content="Plateforme incroyable de e-commerce">
+let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet" />
-    <link href="../css/cart.css" rel="stylesheet" />
+// On récupère les articles avec une fonction puis innerHTML pour les transférer sur la page du panier
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
+function recupPanier() {
+  if (produitLocalStorage === null || produitLocalStorage == 0) {
+    let panierVide = `<p>Avez-vous oublié quelque-chose ? Votre panier est vide.</p>`;
+    document.querySelector("#cart__items").innerHTML = panierVide;
+  } else { 
+    for (let produit in produitLocalStorage) {
+      let ArticleCart = document.querySelector("#cart__items");
+      totalPrice = 
+        produitLocalStorage[produit].prixProduit *
+        produitLocalStorage[produit].quantiteProduit;
+      ArticleCart.innerHTML += `
+    <article class="cart__item" data-id="${produitLocalStorage[produit].idProduit}">
+      <div class="cart__item__img">
+        <img src="${produitLocalStorage[produit].imgProduit}" alt="${produitLocalStorage[produit].altImgProduit}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__titlePrice">
+        <h2>${produitLocalStorage[produit].nomProduit} - ${produitLocalStorage[produit].couleurProduit}</h2>
+        <p>${totalPrice} €</p>
+        </div>
+        <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produitLocalStorage[produit].quantiteProduit}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+        </div>
+      </div>
+      </article>
+      `;
+    }
+  }
+}
+recupPanier();
 
-  <body>
-    <header>
-      <div class="limitedWidthBlockContainer informations">
-        <div class="limitedWidthBlock">
-          <ul>
-            <li><img src="../images/icons/phone.svg" alt="logo de téléphone" class="informations__phone">01 23 45 67 89</li>
-            <li><img src="../images/icons/mail.svg" alt="logo d'une enveloppe" class="informations__mail">support@name.com</li>
-            <li><img src="../images/icons/adress.svg" alt="logo d'un point de géolocalisation" class="informations__address">01 23 45 67 89</li>
-          </ul>
-        </div>
-      </div>
-      <div class="limitedWidthBlockContainer menu">
-        <div class="limitedWidthBlock">
-          <a href="./index.html">
-            <img class="logo" src="../images/logo.png" alt="Logo de l'entreprise">
-          </a>
-          <nav>
-            <ul>
-              <a href="./index.html"><li>Accueil</li></a>
-              <a href="./cart.html"><li>Panier</li></a>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <img class="banniere" src="../images/banniere.png" alt="Baniere">
-    </header>
-    
-    <main class="limitedWidthBlockContainer">
-      <div class="limitedWidthBlock" id="limitedWidthBlock">
-        <div class="cartAndFormContainer" id="cartAndFormContainer">
-          <h1>Votre panier</h1>
-          <section class="cart">
-            <section id="cart__items">
-             <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-                <div class="cart__item__img">
-                  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>Nom du produit</h2>
-                    <p>Vert</p>
-                    <p>42,00 €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article> -->
-            </section>
-            <div class="cart__price">
-              <p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
-            </div>
-            <div class="cart__order">
-              <form method="get" class="cart__order__form">
-                <div class="cart__order__form__question">
-                  <label for="firstName">Prénom: </label>
-                  <input type="text" name="firstName" id="firstName" required>
-                  <p id="firstNameErrorMsg"><!-- ceci est un message d'erreur --></p>
-                </div>
-                <div class="cart__order__form__question">
-                  <label for="lastName">Nom: </label>
-                  <input type="text" name="lastName" id="lastName" required>
-                  <p id="lastNameErrorMsg"></p>
-                </div>
-                <div class="cart__order__form__question">
-                  <label for="address">Adresse: </label>
-                  <input type="text" name="address" id="address" required>
-                  <p id="addressErrorMsg"></p>
-                </div>
-                <div class="cart__order__form__question">
-                  <label for="city">Ville: </label>
-                  <input type="text" name="city" id="city" required>
-                  <p id="cityErrorMsg"></p>
-                </div>
-                <div class="cart__order__form__question">
-                  <label for="email">Email: </label>
-                  <input type="email" name="email" id="email" required>
-                  <p id="emailErrorMsg"></p>
-                </div>
-                <div class="cart__order__form__submit">
-                  <input type="submit" value="Commander !" id="order">
-                </div>
-              </form>
-            </div>
-          </section>
-        </div>
-      </div>
-    </main>
-    
-    <footer>
-      <div class="limitedWidthBlockContainer footerMain">
-        <div class="limitedWidthBlock">
-          <div>
-            <img class="logo" src="../images/logo.png" alt="Logo de l'entreprise">
-          </div>
-          <div>
-            <p>10 quai de la charente <br>75019 Paris 19</p>
-          </div>
-          <div>
-            <p>Téléphone : 01 23 45 67 89</p>
-          </div>
-          <div>
-            <p>Email : support@name.com</p>
-          </div>
-        </div>
-      </div>
-      <div class="limitedWidthBlockContainer footerSecondary">
-        <div class="limitedWidthBlock">
-          <p>© Copyright 2021 - 2042 | Openclassrooms by Openclassrooms | All Rights Reserved | Powered by <3</p>
-        </div>
-      </div>
-    </footer>
-  <script src="../js/cart.js"></script>
-  </body>
-</html>
+// On modifie la quantité dans le panier avec une fonction.
+
+function modifQuantite() {
+  let quantityModif = document.getElementsByClassName("itemQuantity");
+  for (let k = 0; k < quantityModif.length; k++) {
+    quantityModif[k].addEventListener("change", (event) => {
+      if (quantityModif[k] = quantityModif[k]) {
+        let quantityModifs = produitLocalStorage[k].quantiteProduit;
+        let quantityModifValue = quantityModif[k].valueAsNumber;
+
+        const resultFind = produitLocalStorage.filter(
+          (resultat) => resultat.quantityModifValue !== quantityModifs
+
+        );
+
+        resultFind.quantiteProduit = quantityModifValue;
+        produitLocalStorage[k].quantiteProduit = resultFind.quantiteProduit;
+
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        location.reload();
+      }
+    });
+  }
+}
+modifQuantite();
+
+// On affiche le coût total des article via une formule dans une fonction
+
+function total() {;
+  let elementQuantity = document.querySelectorAll(".itemQuantity");
+  if (elementQuantity === null) {
+    return;
+  } else {
+    let lengthElements = elementQuantity.length;
+    totalQuantity = 0;
+    for (var i = 0; i < lengthElements; ++i) {
+      totalQuantity += elementQuantity[i].valueAsNumber;
+    }
+    totalPrice = 0;
+
+    for (var i = 0; i < lengthElements; ++i) {
+      totalPrice +=
+        elementQuantity[i].valueAsNumber * produitLocalStorage[i].prixProduit;
+    }
+
+    let productTotalQuantity = document.querySelector(".cart__price");
+    productTotalQuantity.innerHTML = `<p>Total (<span id="totalQuantity>${totalQuantity}</span> articles) : <span id="totalPrice">${totalPrice}</span> €)</p>`;
+  }
+}
+total();
+
+// on créé une fonction qui permet de supprimer un article du panier
+
+function suppProduit() {
+  let deleteButton = document.querySelectorAll(".deleteItem");
+  let elementQuantities = document.querySelector(".itemQuantity");
+
+  if (elementQuantities === null) {
+    return;
+  } else {
+    let lengthElements = elementQuantities.length;
+    for (var i = 0; i < lengthElements; ++i) {
+      totalQuantities += elementQuantities[i].valueAsNumber;
+    }
+    totalQuantities = elementQuantities;
+    console.log(totalQuantities);
+
+    for (let a = 0; a < deleteButton.length; a++) {
+      deleteButton[a].addEventListener("click", () => {
+        let deleteId = produitLocalStorage[a].idProduit;
+        let deleteColor = produitLocalStorage[a].couleurProduit;
+        let deleteName = produitLocalStorage[a].nomProduit;
+
+        produitLocalStorage = produitLocalStorage.filter(
+          (resultat) =>
+            resultat.idProduit !== deleteId ||
+            resultat.couleurProduit !== deleteColor ||
+            resultat.nomProduit !== deleteName
+        );
+
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+
+        alert("L'article a été retiré du panier");
+        location.reload();
+      });
+    }
+  }
+}
+suppProduit();
+
+// On vérifie avec des regex, les input utilisateur
+
+let form = document.querySelector(".cart__order__form");
+
+let validEmail = false;
+let validCity = false;
+let validAddress = false;
+let validLastName = false;
+let validFirstName = false;
+
+let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+let lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+let addressErrorMsg = document.getElementById("addressErrorMsg");
+let cityErrorMsg = document.getElementById("cityErrorMsg");
+let emailErrorMsg = document.getElementById("emailErrorMsg");
+
+form.firstName.addEventListener("input", function () {
+  let reg = /[a-zA-Z]+/;
+  let str = form.firstName.value;
+  if(reg.test(str)) {
+    validFirstName = true;
+  }
+  else {
+    validFirstName = false;
+  }
+})
+
+form.lastName.addEventListener("input", function () {
+  let reg = /[a-zA-Z]+/;
+  let str = form.lastName.value;
+  if(reg.test(str)) {
+    validLastName = true;
+  }
+  else {
+    validLastName = false;
+  }
+})
+
+form.address.addEventListener("input", function () {
+  let reg = /[0-9]+ ([a-zA-Z]+( [a-zA-Z]+)+)/;
+  let str = form.address.value;
+  if(reg.test(str)) {
+    validAddress = true;
+  }
+  else {
+    validAddress = false;
+  }
+})
+
+form.city.addEventListener("input", function () {
+  let reg = /[a-zA-Z]+/;
+  let str = form.city.value;
+  if(reg.test(str)) {
+    validCity = true;
+  }
+  else {
+    validCity = false;
+  }
+})
+
+form.email.addEventListener("input", function () {
+  let reg = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+  let str = form.email.value;
+  if(reg.test(str)) {
+    validEmail = true;
+  }
+  else {
+    validEmail = false;
+  }
+})
+
+// On vérifie les regex au click souris et on indique si c'est valide ou pas
+
+let button_order = document.getElementById("order")
+button_order.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if(!produitLocalStorage || produitLocalStorage.length < 1 ){
+    alert("Le panier est vide");
+  }else {
+    if (validAddress === true && validLastName === true && validFirstName === true && validEmail === true  && validCity === true ) {
+      postForm();
+    }
+    if (validEmail === false ) {
+      emailErrorMsg.innerHTML = `<p>Le champ Email est mal renseigné</p>`;
+    }
+    else if (validEmail === true) {
+      emailErrorMsg.innerHTML = `<p></p>`;
+    }
+    if (validAddress === false ) {
+      addressErrorMsg.innerHTML = `<p>Le champ Address est mal renseigné</p>`;
+    }
+    else if (validAddress === true) {
+      addressErrorMsg.innerHTML = `<p></p>`;
+    }
+    if (validCity === false ) {
+      cityErrorMsg.innerHTML = `<p>Le champ Ville est mal renseigné</p>`;
+    }
+    else if (validCity === true) {
+      cityErrorMsg.innerHTML = `<p></p>`;
+    }
+    if (validLastName === false ) {
+      lastNameErrorMsg.innerHTML = `<p>Le champ Nom est mal renseigné</p>`;
+    }
+    else if (validLastName === true) {
+      lastNameErrorMsg.innerHTML = `<p></p>`;
+    }
+    if (validFirstName === false ) {
+      firstNameErrorMsg.innerHTML = `<p>Le champ Prénom est mal renseigné</p>`;
+    }
+    else if (validFirstName === true) {
+      firstNameErrorMsg.innerHTML = `<p></p>`;
+    }
+  }
+});
+
+//On envoie la requête post après la validation des input via une fonction qui envoie vers le panier
+
+function postForm() {
+  let inputName = document.getElementById("firstName");
+  let inputLastName = document.getElementById("lastName");
+  let inputAdress = document.getElementById("address");
+  let inputCity = document.getElementById("city");
+  let inputMail = document.getElementById("email");
+  let idProducts = [];
+  for (let i = 0; i < produitLocalStorage.length; i++) {
+    idProducts.push(produitLocalStorage[i].idProduit);
+  }
+  console.log(idProducts);
+
+  const order = {
+    contact: {
+      firstName: inputName.value,
+      lastName: inputLastName.value,
+      address: inputAdress.value,
+      city: inputCity.value,
+      email: inputMail.value,
+    },
+    products: idProducts,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(order),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+
+// On utilise la methode post dans le fetch en retournant les infos de commande
+  fetch("http://localhost:3000/api/products/order", options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      window.localStorage.clear(); // on efface le localStorage
+      document.location.href = "confirmation.html?id=" + data.orderId;
+    })
+    .catch((err) => {
+      alert("Problème avec fetch : " + err.message);
+    });
+}
+
+// On efface le localStorage via une fonction pour ne pas garder les informations et pour permettre de passer une autre commande.
+
+function clearLocalStorage() {
+  window.localStorage.clear();
+  location.reload();
+}
