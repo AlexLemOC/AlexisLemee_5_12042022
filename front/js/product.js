4d4601c54931a4cb5fd0bbeaeee76ba084ae2b54
Alexis Lemée <96991663+AlexLemOC@users.noreply.github.com>
2023-03-16 17:02:47 +0100

    Final update

--- front/js/product.js
+++ front/js/product.js
@@ -1,113 +1,116 @@
-//Identification de l'URL et récupération de l'id
-var str = window.location.href;
-var url = new URL(str);
-var idProduit = url.searchParams.get("_id");
-console.log(idProduit);
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Nom du produit</title>
 
-//déclarations pour le panier
-const couleurChoisie = document.querySelector("#colors");
-const quantiteChoisie = document.querySelector("#quantity");
-let product = "";
+    <meta charset="utf-8">
+    <meta name="description" content="Plateforme incroyable de e-commerce">
 
-fetch("http://localhost:3000/api/products/" + idProduit)
-    .then ((res) => res.json())
-    .then (async(objetProduit) => {
-        console.table(objetProduit);
-        product = await objetProduit;
-        leKanap(objetProduit);
-    })
-    .catch((err) => {
-        document.querySelector(".item").innerHTML = "<h1>erreur 404</h1>";
-        console.log("erreur 404 sur ressource api "+err);
-    });
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet" />
+    <link href="../css/product.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="../images/favicon-32x32.png">
 
-//fonction d'affichage du produit
-function leKanap(product) {
-    //déclaration de la zone image et insertion img + alt
-    let articleImg = document.querySelector(".item__img");
-    articleImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
 
-    let articleNamePrice = document.querySelector(".item__content__titlePrice");
-    articleNamePrice.innerHTML = `<h1 id="title">${product.name}</h1>
-    <p>Prix : <span id="price">${product.price}</span>€</p>`;
-    document.title = product.name;
+  <body>
+    <header>
+      <div class="limitedWidthBlockContainer informations">
+        <div class="limitedWidthBlock">
+          <ul>
+            <li><img src="../images/icons/phone.svg" alt="logo de téléphone" class="informations__phone">01 23 45 67 89</li>
+            <li><img src="../images/icons/mail.svg" alt="logo d'une enveloppe" class="informations__mail">support@name.com</li>
+            <li><img src="../images/icons/adress.svg" alt="logo d'un point de géolocalisation" class="informations__address">01 23 45 67 89</li>
+          </ul>
+        </div>
+      </div>
+      <div class="limitedWidthBlockContainer menu">
+        <div class="limitedWidthBlock">
+          <a href="./index.html">
+            <img class="logo" src="../images/logo.png" alt="Logo de l'entreprise">
+          </a>
+          <nav>
+            <ul>
+              <a href="./index.html"><li>Accueil</li></a>
+              <a href="./cart.html"><li>Panier</li></a>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <img class="banniere" src="../images/banniere.png" alt="Baniere">
+    </header>
 
-    let articleDescription = document.querySelector("#description");
-    articleDescription.innerHTML = product.description;
+    <main class="limitedWidthBlockContainer">
+      <div class="limitedWidthBlock">
+        <section class="item">
+          <article>
+            <div class="item__img">
+              <!-- <img src="../images/logo.png" alt="Photographie d'un canapé"> -->
+            </div>
+            <div class="item__content">
 
-    for (let couleur of product.colors) {
-        console.table(couleur);
-        let articleColors = document.createElement("option");
-        document.querySelector("#colors").appendChild(articleColors);
-        articleColors.value = couleur;
-        articleColors.innerHTML = couleur;
-    }
-    ajoutCart(product);
-}
+              <div class="item__content__titlePrice">
+                <h1 id="title"><!-- Nom du produit --></h1>
+                <p>Prix : <span id="price"><!-- 42 --></span>€</p>
+              </div>
 
-//fonction du panier (Cart)
-function ajoutCart(product) {
-    const btnAddCart = document.querySelector("#addToCart");
+              <div class="item__content__description">
+                <p class="item__content__description__title">Description :</p>
+                <p id="description"><!-- Dis enim malesuada risus sapien gravida nulla nisl arcu. --></p>
+              </div>
+
+              <div class="item__content__settings">
+                <div class="item__content__settings__color">
+                  <label for="color-select">Choisir une couleur :</label>
+                  <select name="color-select" id="colors">
+                      <option value="">--SVP, choisissez une couleur --</option>
+<!--                       <option value="vert">vert</option>
+                      <option value="blanc">blanc</option> -->
+                  </select>
+                </div>
+
+                <div class="item__content__settings__quantity">
+                  <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+                  <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+                </div>
+              </div>
+
+              <div class="item__content__addButton">
+                <button id="addToCart">Ajouter au panier</button>
+              </div>
+
+            </div>
+          </article>
+        </section>
+      </div>
+    </main>
     
-    //écoute du bouton pour ajoueter au panier
-    btnAddCart.addEventListener('click', (event)=>{
-        if (quantiteChoisie.value > 0 && quantiteChoisie.value <=100 && couleurChoisie.value != 0){
-            
-            let colorPicked = couleurChoisie.value;
-
-            let quantityPicked = quantiteChoisie.value;
-
-            //création de la liste avec le mot "produit" pour chaque ligne pour le localStorage
-            let detailProduit = {
-                idProduit: idProduit,
-                couleurProduit: colorPicked,
-                quantiteProduit: Number(quantityPicked),
-                nomProduit: product.name,
-                prixProduit: product.price,
-                descriptionProduit: product.description,
-                imgProduit: product.imageUrl,
-                altImgProduit: product.altTxt
-            };
-
-    
-            let articleLocalStorage = JSON.parse(localStorage.getItem("produit"));
-
-            const popupConfirmation =() =>{
-                if(window.confirm(`Vous avez ajouté ${quantityPicked} ${product.name} ${colorPicked} au panier 
-Pour voir le contenu de votre panier, cliquez sur OK`)){
-                    window.location.href ="cart.html";
-                }
-            }
-
-            if (articleLocalStorage) {
-            const resultFind = articleLocalStorage.find(
-                (kanap) => kanap.idProduit === idProduit && kanap.couleurProduit === colorPicked);
-                //Si le produit commandé existe déjà dans le panier
-                if (resultFind) {
-                    let newQuantite =
-                    parseInt(detailProduit.quantiteProduit) + parseInt(resultFind.quantiteProduit);
-                    resultFind.quantiteProduit = newQuantite;
-                    localStorage.setItem("produit", JSON.stringify(articleLocalStorage));
-                    console.table(articleLocalStorage);
-                    popupConfirmation();
-                //Si le produit commandé est un nouveau produit dans le panier
-                } else {
-                    articleLocalStorage.push(detailProduit);
-                    localStorage.setItem("produit", JSON.stringify(articleLocalStorage));
-                    console.table(articleLocalStorage);
-                    popupConfirmation();
-                }
-            //Si le panier est vide
-            } else {
-                articleLocalStorage =[];
-                articleLocalStorage.push(detailProduit);
-                localStorage.setItem("produit", JSON.stringify(articleLocalStorage));
-                console.table(articleLocalStorage);
-                popupConfirmation();
-            }
-        } else {
-            window.confirm(`Pour ajouter un article au panier, 
-veuillez choisir une couleur et une quantité.`)
-        }
-    });
-}
+    <footer>
+      <div class="limitedWidthBlockContainer footerMain">
+        <div class="limitedWidthBlock">
+          <div>
+            <img class="logo" src="../images/logo.png" alt="Logo de l'entreprise">
+          </div>
+          <div>
+            <p>10 quai de la charente <br>75019 Paris 19</p>
+          </div>
+          <div>
+            <p>Téléphone : 01 23 45 67 89</p>
+          </div>
+          <div>
+            <p>Email : support@name.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="limitedWidthBlockContainer footerSecondary">
+        <div class="limitedWidthBlock">
+          <p>© Copyright 2021 - 2042 | Openclassrooms by Openclassrooms | All Rights Reserved | Powered by <3</p>
+        </div>
+      </div>
+    </footer>
+  <script src="../js/product.js"></script>
+  </body>
+</html>
