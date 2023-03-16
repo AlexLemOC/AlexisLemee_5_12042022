9a98813cf9065bf2b30dab41c8f53d7afd5ba412
Alexis Lemée <96991663+AlexLemOC@users.noreply.github.com>
2023-03-16 16:32:28 +0100

    Update final

--- front/js/script.js
+++ front/js/script.js
@@ -1,43 +1,37 @@
-// Quand la page charge, on appel la fonction asynchrone.
-
-window.onload = fillSection();
-const urlProduct = "http://localhost:3000/api/products";
-
-// On récupère les articles dans le back de manière asynchrone
-
-async function getArticles() {
-  let articlesCatch = await fetch(urlProduct)
-  return await articlesCatch.json();
+// Récupération des produits de l'api
+//------------------------------------------------------------------------
+fetch("http://localhost:3000/api/products")
+  // quand tu as la réponse donne le résultat en json.
+  .then((res) => res.json())
+  // ce que l'on a reçu et qui a été traité en json sera appelé objetProduits
+  .then((objetProduits) => {
+    // donne moi des informations en console sur ce qui est récupéré sous forme tableau.
+    console.table(objetProduits);
+    // appel de la fonction d'affichage des produits
+    lesKanaps(objetProduits);
+  })
+  // dans le cas d'une erreur remplace le contenu de titre par un h1 au contenu de erreur 404 et renvoit en console l'erreur.
+  .catch((err) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404, sur ressource api:" + err);
+  });
+//----------------------------------------------------------------------
+// fonction d'affichage des produits de l'api sur la page index
+//----------------------------------------------------------------------
+function lesKanaps(index) {
+  // déclaration de variable de la zone d'article
+  let zoneArticle = document.querySelector("#items");
+  // boucle pour chaque indice(nommé 'article') dans index
+  for (let article of index) {
+    /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin      
+    produit + paramètres(son id);*/
+ 
+    zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+    <article>
+      <img src="${article.imageUrl}" alt="${article.altTxt}">
+      <h3 class="productName">${article.name}</h3>
+      <p class="productDescription">${article.description}</p>
+    </article>
+  </a>`;
+  }
 }
-
-// On appel la liste de produits quand la page a chargé
-
-async function fillSection() {
-  setTimeout(() => {
-    getListProduct;
-  }, 500);
-}
-
-// On fait une promesse en utilisant le timer pour afficher la liste des produits
-
-const getListProduct = new Promise(() => {
-  setTimeout(() => {
-    let result = getArticles ()
-    .then(function (data){
-      const articles = data;
-      for (let article in articles) {
-          let ZoneArticles = document.getElementById("items");
-          ZoneArticles.innerHTML += `<a href="./product.html?id=${data[article]._id}">
-          <article>
-            <img src="${data[article].imageUrl}" alt="${data[article].altTxt}">
-            <h3 class="productName">${data[article].name}</h3>
-            <p class="productDescription">${data[article].description}</p>
-          </article>
-        </a><img>`
-      }
-    })
-    .catch (function(error){
-      return error;
-    });
-  }, 300);
-});
